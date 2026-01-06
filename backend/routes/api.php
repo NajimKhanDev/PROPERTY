7da6e037de46bf96b627b3ad4d5125b42f1e904a
Faijal Khan <faizal@alibrosinfotech.com>
2026-01-06 11:54:53 +0530

    added properties migration, model, and controller

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\PropertyController;
+use App\Http\Controllers\DocumentController;
 
 
 
